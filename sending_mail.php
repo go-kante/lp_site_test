a3d898c39d5748d61213363e95027c855155044f
Kanta Sato <163727540+go-kante@users.noreply.github.com>
2025-08-07 00:24:38 +0900

    phpメール機能

--- sending_mail.php
+++ sending_mail.php
@@ -1,6 +1,7 @@
 <?php
 
 // フォームから送信されたデータを取得
+$mail = $_POST['mail'];
 $sei = $_POST['sei'];
 $mei = $_POST['mei'];
 $gender = $_POST['gender'];
@@ -8,15 +9,17 @@ $content = $_POST['content'];
 
 // 件名と本文を作成
 $subject = "ウェブサイトからの問い合わせ";
+$body = "以下の内容でお問い合わせがありました。\n\n";
+$body .= "メールアドレス： " . $mail . "\n";
 $body = "氏名： " . $sei . " " . $mei . "\n";
 $body .= "性別： " . $gender . "\n";
 $body .= "問い合わせ内容：\n" . $content;
 
 // 送信先のメールアドレス
-$to = "alpharustest@gmail.com"; // ここに送信先のメールアドレスを入力してください
+$to = "info@alpharastest.shop"; // ここに送信先のメールアドレスを入力してください
 
 // ヘッダー情報
-$headers = "From: \アルファラス \n"; // ここに送信元のアドレスを入力してください
+$headers = "From: info@alpharastest.shop \n"; // ここに送信元のアドレスを入力してください
 $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
 // メール送信
