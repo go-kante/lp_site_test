36e743a9724a3a42ce200fc5c59ac7da0d0c9cdb
Kanta Sato <163727540+go-kante@users.noreply.github.com>
2025-08-06 13:52:38 +0900

    phpファイル追加

---
+++ sending_mail.php
@@ -0,0 +1,29 @@
+<?php
+
+// フォームから送信されたデータを取得
+$sei = $_POST['sei'];
+$mei = $_POST['mei'];
+$gender = $_POST['gender'];
+$content = $_POST['content'];
+
+// 件名と本文を作成
+$subject = "ウェブサイトからの問い合わせ";
+$body = "氏名： " . $sei . " " . $mei . "\n";
+$body .= "性別： " . $gender . "\n";
+$body .= "問い合わせ内容：\n" . $content;
+
+// 送信先のメールアドレス
+$to = "alpharustest@gmail.com"; // ここに送信先のメールアドレスを入力してください
+
+// ヘッダー情報
+$headers = "From: \アルファラス \n"; // ここに送信元のアドレスを入力してください
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+// メール送信
+if (mb_send_mail($to, $subject, $body, $headers)) {
+    echo "メールが正常に送信されました。";
+} else {
+    echo "メールの送信に失敗しました。";
+}
+
+?>
